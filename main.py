3b5be817cd02d3eacfa24c62b38d6bcb0378c4d9
MaximUrban <64153150+MaximUrban@users.noreply.github.com>
2025-04-22 11:27:53 +0300

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import os
 from telethon import TelegramClient, events
 
-# Получаем данные из переменных окружения
-api_id = int(os.environ['26861283'])
-api_hash = os.environ['25b1a58fc5efb377b2dfb02672fdd9c5']
-bot_token = os.environ['7472984810:AAHOROGgrsl1-bNRrni3Vt9ya5oY2onEEqM']
+# Вставленные данные напрямую
+api_id = 26861283
+api_hash = '25b1a58fc5efb377b2dfb02672fdd9c5'
+bot_token = '7472984810:AAHOROGgrsl1-bNRrni3Vt9ya5oY2onEEqM'
 
-# Создаем клиент
+# Создаем клиента
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
-# Пример обработчика входящих сообщений
+# Обработчик входящих сообщений
 @client.on(events.NewMessage)
 async def handler(event):
     sender = await event.get_sender()
@@ -17,7 +16,7 @@ async def handler(event):
     print(f"Сообщение от {sender.id}: {message}")
     await event.reply("Привет! Бот работает на Render 🎉")
 
-# Запускаем клиента
+# Запуск клиента
 with client:
     print("Бот запущен.")
     client.run_until_disconnected()
